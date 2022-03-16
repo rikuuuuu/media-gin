c7d7408b3ff32246954f3f9eede7ae6450146afa
rikuuuuu <silk.riku0369@gmail.com>
2022-03-16 21:28:01 +0900

    add test repository

---
+++ Makefile
@@ -0,0 +1,2 @@
+run:
+	go run app/server.go
---
+++ app/server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+    engine := gin.Default()
+
+    testEngine := engine.Group("/test")
+    {
+        v1 := testEngine.Group("/v1")
+        {
+            v1.GET("/test", func(ctx *gin.Context) {
+				ctx.String(200, "Hello, World!")
+			})
+        }
+    }
+	
+    err := engine.Run("127.0.0.1:3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
